4be9f4ae7c711706fd9739518a2bf73e68936bdd
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-12-23 13:16:35 +0900

    feat: Read raw problems and pick a problem

---
+++ app/src/main/java/com/github/mutoxu_n/sudoku/Utils.kt
@@ -0,0 +1,19 @@
+package com.github.mutoxu_n.sudoku
+
+import android.util.Log
+import java.io.InputStream
+
+private const val DATA_SIZE = 83
+fun choiceProblem(stream: InputStream): String {
+    val size = stream.available()
+    val len = size / DATA_SIZE
+    val idx = (Math.random() * len).toInt()
+
+    val reader = stream.bufferedReader()
+    reader.skip(83L * idx)
+
+    val p = reader.readLine()
+    Log.i("choiceProblem", "Index: $idx / $len")
+    Log.i("choiceProblem", "problem: $p")
+    return p
+}
