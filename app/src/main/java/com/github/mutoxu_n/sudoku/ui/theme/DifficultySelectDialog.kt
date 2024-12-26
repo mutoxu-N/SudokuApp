c43dc76d4e22c1d894df6d19fc220d6f6d9f1d51
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-12-26 09:18:43 +0900

    feat: Show number keyboard on inputting problem id

--- app/src/main/java/com/github/mutoxu_n/sudoku/ui/theme/DifficultySelectDialog.kt
+++ app/src/main/java/com/github/mutoxu_n/sudoku/ui/theme/DifficultySelectDialog.kt
@@ -3,6 +3,7 @@ package com.github.mutoxu_n.sudoku.ui.theme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,6 +20,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.mutoxu_n.sudoku.Difficulty
@@ -31,7 +34,7 @@ fun DifficultySelectDialog(
     onConfirm: (Difficulty, Int) -> Unit,
 ) {
     var difficulty by remember { mutableStateOf(Difficulty.EASY) }
-    var problemNum by remember { mutableIntStateOf(0) }
+    var problemNum by remember { mutableIntStateOf(-1) }
 
     AlertDialog(
         title = { Text(text = stringResource(R.string.dialog_difficulty_title)) },
@@ -51,9 +54,16 @@ fun DifficultySelectDialog(
                 Spacer(Modifier.size(16.dp))
 
                 TextField(
-                    value = problemNum.toString(),
-                    onValueChange = { problemNum = it.toIntOrNull() ?: 0 },
-                    label = { Text(text = stringResource(R.string.dialog_textfield_problem_id)) }
+                    value = if(problemNum == -1) "" else problemNum.toString(),
+                    onValueChange = {
+                        val new = it.toIntOrNull()
+                        if(new != null) problemNum = it.toInt()
+                    },
+                    label = { Text(text = stringResource(R.string.dialog_textfield_problem_id)) },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Decimal,
+                        imeAction = ImeAction.Done,
+                    )
                 )
             }
         },
